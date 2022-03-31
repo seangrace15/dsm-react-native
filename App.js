51acc4db8f81e3a1ace5d1d8f2538538dd96a54c
Sean Grace <seangrace@MacBook-Pro-de-Sean.local>
2022-03-31 15:40:39 +0200

    Componentes React Native

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import Campobase from './componentes/CampoBaseComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <Campobase/>
       <StatusBar style="auto" />
-    </View>
-  );
+</View> );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
